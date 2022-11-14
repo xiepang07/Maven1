72742ad8d197b3a1b5c983e22d9ef6dc030947c8
xiepang07 <103099336+xiepang07@users.noreply.github.com>
2022-11-14 22:10:30 +0800

    gihub do1

--- src/main/java/com/xiepang/GitTest.java
+++ src/main/java/com/xiepang/GitTest.java
@@ -3,5 +3,6 @@ package com.xiepang;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git,again1");
+        System.out.println("hello git,again2");
     }
 }
