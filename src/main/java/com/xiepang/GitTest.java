92caa6ee52e6baced83fcb253169cbac7b41f362
xiepang <1147422477@qq.com>
2022-11-14 20:12:29 +0800

    第一次 idea提交

--- src/main/java/com/xiepang/GitTest.java
+++ src/main/java/com/xiepang/GitTest.java
@@ -2,6 +2,6 @@ package com.xiepang;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git,again");
     }
 }
